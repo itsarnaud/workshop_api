a4b4947eee459b44e8e418434e910a580b1b8289
Arnaud R. <arno.royer2004@gmail.com>
2025-10-06 16:24:15 +0200

    Feat: games crud

--- controllers/GamesController.js
+++ controllers/GamesController.js
@@ -1,4 +1,22 @@
+const { prisma } = require('../db/prisma');
+
 module.exports.index = async (req, res) => {
-  res.json({ msg: 'games' })
+  const games = await prisma.game.findMany();
+  res.json(games);
+}
+
+module.exports.create = async (req, res) => {
+  await prisma.game.create({ data: req.body });
+  res.json({ msg: 'ok' });
+}
+
+module.exports.show = async (req, res) => {
+  const game = await prisma.game.findUnique({ where: { id: req.body.id } });
+  res.json(game);
+}
+
+module.exports.update = async (req, res) => {
+  const game = await prisma.game.update({ where: { id: req.body.id }, data: req.body })
+  res.json(game);
 }
 
--- routes/games.js
+++ routes/games.js
@@ -3,6 +3,9 @@ const router  = express.Router()
 
 const GamesController = require('../controllers/GamesController')
 
-router.get('/', GamesController.index)
+router.get ('/',    GamesController.index);
+router.post('/',    GamesController.create);
+router.get ('/:id', GamesController.show);
+router.put ('/:id', GamesController.update);
 
 module.exports = router
